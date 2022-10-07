a858d8f74de4c2e944b0550bf9ea808ffc6de55f
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2022-10-07 17:24:37 +0530

    Adding and Styling Meals

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,9 +1,23 @@
 import { useGlobalContext } from "../context";
 
 const Meals = () => {
-  const context = useGlobalContext();
-  console.log(context);
-  return <h1>Hi! Meals</h1>;
+  const { meals } = useGlobalContext();
+  return (
+    <section className="section-center">
+      {meals.map((singleMeal) => {
+        const { idMeal, strMeal: title, strMealThumb: image } = singleMeal;
+        return (
+          <article key={idMeal} className="single-meal">
+            <img src={image} className="img" />
+            <footer>
+              <h5>{title}</h5>
+              <button className="like-btn">Click Me</button>
+            </footer>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Meals;
