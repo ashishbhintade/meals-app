fbaa27d79831c0c8a2bad2fba647a720bd3de501
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2022-10-11 16:24:34 +0530

    Search Functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,12 +2,28 @@ import { useState } from "react";
 import { useGlobalContext } from "../context";
 
 const Search = () => {
+  const [text, setText] = useState("");
+
+  const { setSearchTerm } = useGlobalContext();
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text) {
+      setSearchTerm(text);
+    }
+  };
   return (
     <header className="search-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Type Favorite Meal"
+          value={text}
+          onChange={handleChange}
           className="form-input"
         />
         <button type="submit" className="btn">
